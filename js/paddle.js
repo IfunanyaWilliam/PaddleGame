1fb3c338774ffe227f2e164d7ed9561899c302f6
IfunanyaWilliam <williamifunanya@gmail.com>
2022-11-21 00:56:58 +0100

    Add ball.js

--- js/paddle.js
+++ js/paddle.js
@@ -1,5 +1,9 @@
 export default class Paddle {
     constructor(gameWidth, gameHeight){
+
+        this.gameWidth = gameWidth;
+
+        //This width and Height refer to the paddle
         this.width = 150;
         this.height = 30;
 
@@ -18,15 +22,31 @@ export default class Paddle {
         this.speed = -this.maxSpeed;
     }
 
+    moveRight(){
+        this.speed = this.maxSpeed;
+    }
+
+    stop(){
+        this.speed = 0;
+    }
+
     draw(ctx){
         ctx.fillStyle = '#0ff';
         ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
 
     update(deltaTime){
-        if(!deltaTime) return;
 
         this.position.x += this.speed; 
+
+        if(this.position.x < 0){
+            this.position.x = 0;
+        }
+
+        if(this.position.x + this.width > this.gameWidth){
+            this.position.x = this.gameWidth - this.width;
+        }
+            
     }
 
 }
--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 import Paddle from "./paddle.js";
 import InputHandler from "./input.js";
+import Ball from "./ball.js";
 
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
@@ -13,9 +14,13 @@ const GAME_HEIGHT = 600;
 
 ctx.clearRect(0, 0, 800, 600);
 
-let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT)
+let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+let ball = new Ball(GAME_WIDTH, GAME_HEIGHT);
 
 new InputHandler(paddle);
+
+
+// Images
 
 
 let lastTime = 0;
@@ -26,12 +31,16 @@ function gameLoop(timestamp){
 
     lastTime = timestamp;
 
-    ctx.clearRect(0, 0, 800, 600);
+    ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
     paddle.update(deltaTime)
     paddle.draw(ctx);
+
+    ball.update(deltaTime);
+    ball.draw(ctx);
+    
 
     requestAnimationFrame(gameLoop);
 }
 
 
-gameLoop();
+requestAnimationFrame(gameLoop);
